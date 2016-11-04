4c482339e09bcefda7004a170a36d2d8a91444d9
Alumno <alumno@alumnos-mac.local>
2016-11-04 09:19:59 +0100

    Initial Commit

---
+++ Ejercicio_Multipantalla_03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ejercicio_Multipantalla_03
+//
+//  Created by Alumno on 04/11/16.
+//  Copyright © 2016 Alumno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ejercicio_Multipantalla_03Tests/Ejercicio_Multipantalla_03Tests.m
@@ -0,0 +1,39 @@
+//
+//  Ejercicio_Multipantalla_03Tests.m
+//  Ejercicio_Multipantalla_03Tests
+//
+//  Created by Alumno on 04/11/16.
+//  Copyright © 2016 Alumno. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ejercicio_Multipantalla_03Tests : XCTestCase
+
+@end
+
+@implementation Ejercicio_Multipantalla_03Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
